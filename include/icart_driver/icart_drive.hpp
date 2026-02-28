d789a838edba84ad47dfc3258a55a0d93546642d
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2026-02-28 19:23:56 +0900

    ✨feat: hpp

--- include/icart_driver/icart_drive.hpp
+++ include/icart_driver/icart_drive.hpp
@@ -1,13 +1,36 @@
+#pragma once
+
+#include <chrono>
+
+#include <geometry_msgs/msg/twist.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/empty.hpp>
 
 #include <ypspur.h>
 
 class IcartDriver : public rclcpp::Node
 {
 public:
-    IcartDriver(const rclcpp::NodeOptions & options);
-    ~IcartDriver();
+  explicit IcartDriver(const rclcpp::NodeOptions & options);
+  ~IcartDriver();
+
 private:
-    void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg);
-    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
+  void init_params();
+  void bringup_ypspur();
+  void loop();
+
+  void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg);
+  void restart_callback(const std_msgs::msg::Empty::SharedPtr msg);
+  void emergency_callback(const std_msgs::msg::Empty::SharedPtr msg);
+
+  int interval_ms;
+  double linear_max_vel;
+  double linear_max_acc;
+  double angular_max_vel;
+  double angular_max_acc;
+
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
+  rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr restart_sub_;
+  rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr emergency_sub_;
+  rclcpp::TimerBase::SharedPtr timer_;
 };
