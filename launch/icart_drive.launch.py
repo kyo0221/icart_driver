42a730e9558075a4a650f94e7573d6b6e6546b40
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2026-02-28 19:40:25 +0900

    ✨feat: add launch

---
+++ launch/icart_drive.launch.py
@@ -0,0 +1,42 @@
+import os
+import yaml
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    config_file_path = os.path.join(
+        get_package_share_directory('icart_driver'),
+        'config',
+        'main_params.yaml'
+    )
+
+    with open(config_file_path, 'r') as file:
+        launch_params = yaml.safe_load(file)['launch']['ros__parameters']
+
+    # メイン実行機ノードの作成
+    main_exec_node = Node(
+        package = 'icart_driver',
+        executable = 'icart_drive_main',
+        parameters = [config_file_path],
+        output='screen'
+    )
+
+    # 操縦機ノードの作成
+    joy_node = Node(
+        package = 'joy',
+        executable = 'joy_node',
+        output='screen'
+    )
+
+    # 起動エンティティクラスの作成
+    launch_discription = LaunchDescription()
+
+    # 起動の追加
+    if(launch_params['joy'] is True):
+        launch_discription.add_entity(joy_node)
+
+    launch_discription.add_entity(main_exec_node)
+
+    return launch_discription
