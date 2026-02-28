4e0dad449f440d45dfe38daa8b9d8693e1c526fc
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2026-02-28 19:01:09 +0900

    🚧wip: 作業中

---
+++ scripts/ypspur_coordinator.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+ypspur-coordinator -p $1 -d /dev/sensors/icart_mini --reconnect
--- src/icart_drive.cpp
+++ src/icart_drive.cpp
@@ -3,29 +3,67 @@
 IcartDriver::IcartDriver(const rclcpp::NodeOptions & options)
 : Node("icart_drive", options),
 interval_ms(get_parameter("interval_ms").as_int()),
-linear_max(get_parameter("linear_max.vel").as_double()),
-angular_max(get_parameter("angular_max.vel").as_double())
+linear_max_vel(get_parameter("linear_max.vel").as_double()),
+linear_max_acc(get_parameter("linear_max.acc").as_double()),
+angular_max_vel(get_parameter("angular_max.vel").as_double()),
+angular_max_acc(get_parameter("angular_max.acc").as_double())
 {
     cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
         "cmd_vel", 10, std::bind(&IcartDriver::cmd_vel_callback, this, std::placeholders::_1));
 
-    read_params();
+    restart_sub_ = this->create_subscription<std_msgs::msg::Empty>(
+        "restart", 10, std::bind(&IcartDriver::restart_callback, this));
+
+    emergency_sub_ = this->create_subscription<std_msgs::msg::Empty>(
+        "emergency", 10, std::bind(&IcartDriver::emergency_callback, this));
+    
+    bringup_ypspur();
+
+    timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(interval_ms),
+        std::bind(&IcartDriver::loop, this));
 }
 
 IcartDriver::~IcartDriver()
 {
-    Spur_stop();
+    Spur_free();
 }
 
-IcartDriver::read_params()
+void IcartDriver::init_params()
 {
-    int port;
-    this->declare_parameter("port", 0);
-    this->get_parameter("port", port);
-    Spur_init(port);
+    Spur_set_vel(linear_max_vel);
+    Spur_set_angvel(angular_max_vel);
+    Spur_set_accel(linear_max_acc);
+    Spur_set_angaccel(angular_max_acc);
+
+    Spur_set_pos_GL(0, 0, 0);
+    Spur_free();
+}
+
+void IcartDriver::bringup_ypspur()
+{
+    if(Spur_init()){
+        RCLCPP_INFO(this->get_logger(), "Bring up ypspur");
+        init_params();
+    }else{
+        RCLCPP_ERROR(this->get_logger(), "Disconnected ypspur");
+    }
 }
 
 void IcartDriver::cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg)
 {
     Spur_vel(msg->linear.x, msg->angular.z);
+}
+
+void IcartDriver::restart_callback(const std_msgs::msg::Empty::SharedPtr msg)
+{
+    bringup_ypspur();
+    Spur_unfreeze();
+    RCLCPP_INFO(this->get_logger(), "Restart ypspur");
+}
+
+void IcartDriver::emergency_callback(const std_msgs::msg::Empty::SharedPtr msg)
+{
+    Spur_freeze();
+    RCLCPP_INFO(this->get_logger(), "Emergency stop ypspur");
 }
