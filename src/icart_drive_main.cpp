875524d8f4c6db32f6705ce8247e05405b7e9b94
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2026-02-28 19:24:12 +0900

    🐛fix: バグ修正

--- src/icart_drive_main.cpp
+++ src/icart_drive_main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     nodes_option.allow_undeclared_parameters(true);
     nodes_option.automatically_declare_parameters_from_overrides(true);
 
-    auto drive_node = std::make_shared<icart_driver::IcartDrive>(nodes_option);
+    auto drive_node = std::make_shared<IcartDriver>(nodes_option);
 
     exec.add_node(drive_node);
 
